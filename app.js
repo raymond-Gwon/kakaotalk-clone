4fa47ae055c5e6e3b88bbf9a418a0678898e5a2a
Raymond <kks900027@gmail.com>
2020-12-25 11:51:31 +0900

    make chat server
    not completed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socket = require('socket.io');
 // Node.js 기본 내장 모듈 불러오기
 const http = require('http');
+const fs = require('fs')
 
 // express 객체 생성
 const app = express();
@@ -13,12 +14,34 @@ const server = http.createServer(app);
 // 생성된 서버를 socket.io에 바인딩
 const io = socket(server);
 
+app.use('/css', express.static('./static/css'))
+app.use('/js', express.static('./static/js'))
+
 // get방식으로 / 경로에 접속하면 실행 됨
 app.get('/', function(request, response) {
-    console.log('유저가 / 으로 접속하였습니다!')
-    response.send('Hello, Express Server!!')
+    fs.readFile('./static/chat.html', function(err, data) {
+        if(err) {
+            response.send('에러')
+        } else {
+            response.writeHead(200, {'Content-Type' : 'text/html'})
+            response.write(data)
+            response.end()
+        }
+    })
 })
 
+
+io.server.on('connection', function(socket) {
+    console.log('유저 접속 됨')
+
+    socket.on('send', function(data) {
+        console.log('전달된 메세지: ', data.msg)
+    })
+
+    socket.on('disconnect', function() {
+        console.log('접속 종료')
+    })
+})
 
 // 서버를 8080 포트로 listen
 server.listen(8080,function(){
